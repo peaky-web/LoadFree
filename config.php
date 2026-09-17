0ce9e74d2fd0a0cd7a2ef7323b9564d14597f3c5
Peaky <peakmlbb9@gmail.com>
2026-09-17 23:08:13 +0800

    Show database connection error

--- config.php
+++ config.php
@@ -20,5 +20,5 @@ try {
     $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (PDOException $e) {
     http_response_code(500);
-    die("Database connection failed.");
+    die("Database connection failed: " . $e->getMessage());
 }
